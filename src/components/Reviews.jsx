6e4aae906b7d68c335917e4c46c7ea39fa9074ce
johnbearfc <jclleggett@gmail.com>
2021-10-19 15:51:48 +0100

    add error and loading messages

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -3,11 +3,18 @@ import { useParams } from 'react-router';
 import styled from 'styled-components';
 import { getReviews } from '../utils/api';
 
+const ReviewsWrapper = styled.section`
+    margin: 60px auto;
+    width: 80%;
+
+    h1 {
+        text-transform: capitalize;
+    }
+`
+
 const ReviewsList = styled.ul`
     list-style-type: none;
-    margin: auto;
     padding: 0;
-    width: 80%;
 `
 
 const ListItem = styled.div`
@@ -26,32 +33,55 @@ const ListItem = styled.div`
 
 const Reviews = () => {
     const [reviews, setReviews] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [err, setErr] = useState(null);
     const { category_slug } = useParams();
 
     console.log(category_slug);
 
     useEffect(() => {
+        setErr(null);
+
         getReviews(category_slug).then((reviewsFromApi) => {
             setReviews(reviewsFromApi);
+            setLoading(false);
         })
+        .catch((err) => {
+            setLoading(false);
+            if (err.response.status === 404) setErr('Category not found');
+            else setErr('Something went wrong :(');
+        });
+        
         window.scrollTo(0, 0);
-    }, [category_slug])
+    }, [category_slug]);
+
+    if (err) {
+        return (
+            <ReviewsWrapper>
+                <p>{err}</p>
+            </ReviewsWrapper>
+        )
+    }
 
     return (
-        <ReviewsList>
-            {reviews.map(review => {
-                return (
-                    <li key={review.review_id}>
-                        <ListItem>
-                            <h3>{review.title}</h3>
-                            <img src={review.review_img_url} alt={review.title}/>
-                            <p>{review.owner} | {review.category}</p>
-                            <p>{review.votes} Votes | {review.comment_count} comments</p>
-                        </ListItem>
-                    </li>
-                )
-            })}
-        </ReviewsList>
+        <ReviewsWrapper>
+            <h1>{category_slug || 'All'}</h1>
+            <p>{loading && 'Loading...'}</p>
+            <ReviewsList>
+                {reviews.map(review => {
+                    return (
+                        <li key={review.review_id}>
+                            <ListItem>
+                                <h3>{review.title}</h3>
+                                <img src={review.review_img_url} alt={review.title}/>
+                                <p>{review.owner} | {review.category}</p>
+                                <p>{review.votes} Votes | {review.comment_count} comments</p>
+                            </ListItem>
+                        </li>
+                    )
+                })}
+            </ReviewsList>
+        </ReviewsWrapper>
     )
 }
 
